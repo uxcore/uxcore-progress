47fead7c3803b3043f1ec89bd1890ec5ecaa2182
文生 <biangang.bg@alibaba-inc.com>
2016-12-19 16:27:20 +0800

    ver. 1.0.4

--- HISTORY.md
+++ HISTORY.md
@@ -1,16 +1,20 @@
 # History
 
+## 1.0.4
+- `UPGRADED` to lastest scaffold
+- `FIXED` vertical align of the text with the line.
+
 ## 1.0.3
-`CHANGED` update scaffold
+- `CHANGED` update scaffold
 
 ## 1.0.2
-`CHANGED` now can customize the icon
+- `CHANGED` now can customize the icon
 
 ## 1.0.1
-`CHANGED` replace error & success icon with kuma-icon
+- `CHANGED` replace error & success icon with kuma-icon
 
 ## 1.0.0
-`NEW` first commit
+- `NEW` first commit
 
 
 
--- src/Progress.js
+++ src/Progress.js
@@ -12,67 +12,71 @@ import assign from 'object-assign';
 const prefixCls = 'kuma-progress';
 
 class Line extends React.Component {
-    render(){
-        let props = assign({}, this.props);
-
-        if (parseInt(props.percent, 10) === 100) {
-            props.status = 'success';
-        }
-
-        let progressInfo;
-        let fullCls = '';
-
-        if(props.showInfo === true){
-            if (props.status === 'exception') {
-                progressInfo = (
-                    <span className={`${prefixCls}-line-text`}>
-                        <i className="icon-caution"></i>
-                    </span>
-                );
-            } else if (props.status === 'success') {
-                progressInfo = (
-                    <span className={`${prefixCls}-line-text`}>
-                        <i className="icon-success"></i>
-                    </span>
-                );
-            } else {
-                progressInfo = (
-                    <span className={`${prefixCls}-line-text`}>{props.percent}%</span>
-                );
-            }
-        } else {
-            fullCls = ` ${prefixCls}-line-wrap-full`;
-        }
-
-        let percentStyle = {
-            width: props.percent + '%',
-            height: props.strokeWidth
-        };
-
-        return (
-            <div className={`${prefixCls}-line-wrap fn-clear status-${props.status}${fullCls}`}>
-                {progressInfo}
-                <div className={`${prefixCls}-line-outer`}>
-                    <div className={`${prefixCls}-line-inner`}>
-                        <div className={`${prefixCls}-line-bg`} style={percentStyle}></div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
-Line.displayName = "Progress-Line";
-Line.propTypes = {
+  static displayName = 'Progress-Line';
+  static propTypes = {
     status: React.PropTypes.oneOf(['normal', 'exception', 'active', 'success']),
     showInfo: React.PropTypes.bool,
     percent: React.PropTypes.number,
-    strokeWidth: React.PropTypes.number
-};
-Line.defaultProps = {
+    strokeWidth: React.PropTypes.number,
+  }
+  static defaultProps = {
     percent: 0,
     strokeWidth: 10,
     status: 'normal', // exception active
     showInfo: true
-};
+  }
+  render(){
+    let props = assign({}, this.props);
+
+    if (parseInt(props.percent, 10) === 100) {
+      props.status = 'success';
+    }
+
+    let progressInfo;
+    let fullCls = '';
+
+    if(props.showInfo === true){
+      if (props.status === 'exception') {
+        progressInfo = (
+          <span className={`${prefixCls}-line-text`}>
+            <i className="icon-caution"></i>
+          </span>
+        );
+      } else if (props.status === 'success') {
+        progressInfo = (
+          <span className={`${prefixCls}-line-text`}>
+            <i className="icon-success"></i>
+          </span>
+        );
+      } else {
+        progressInfo = (
+          <span className={`${prefixCls}-line-text`}>{props.percent}%</span>
+        );
+      }
+    } else {
+      fullCls = ` ${prefixCls}-line-wrap-full`;
+    }
+
+    const percentStyle = {
+      width: props.percent + '%',
+      height: props.strokeWidth
+    };
+
+    const outerStyle = {
+      paddingTop: (22 - props.strokeWidth) / 2,
+    };
+
+    return (
+      <div className={`${prefixCls}-line-wrap fn-clear status-${props.status}${fullCls}`}>
+        {progressInfo}
+        <div className={`${prefixCls}-line-outer`} style={outerStyle}>
+          <div className={`${prefixCls}-line-inner`}>
+            <div className={`${prefixCls}-line-bg`} style={percentStyle}></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Line;
